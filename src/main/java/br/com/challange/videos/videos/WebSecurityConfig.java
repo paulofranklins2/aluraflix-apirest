929d433dd068934672bf9e9a91c9467b8e5b70a7
paulofranklins2 <paulofranklinc@gmail.com>
2022-04-12 12:48:00 -0400

    correction .gitignore

--- src/main/java/br/com/challange/videos/videos/WebSecurityConfig.java
+++ src/main/java/br/com/challange/videos/videos/WebSecurityConfig.java
@@ -1,37 +1,61 @@
 package br.com.challange.videos.videos;
 
-//@Configuration
-//@EnableWebSecurity
-public class WebSecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-//    @Autowired
-//    private DataSource dataSource;
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//
-//
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-////                      Criar usuario inicial.
-////        UserDetails user =
-////                User.builder()
-////                        .username("paulo")
-////                        .password(bCryptPasswordEncoder.encode("paulo"))
-////                        .roles("ADM")
-////                        .build();
-//
-//        auth
-//                .jdbcAuthentication()
-//                .dataSource(dataSource)
-//                .passwordEncoder(bCryptPasswordEncoder);
-////                .withUser(user);
-//
-//
-//    }
+import javax.sql.DataSource;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/videos/free").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .httpBasic();
+
+//                .formLogin()
+//                    .loginPage("/login")
+//                    .permitAll()
+//                    .and()
+//                .logout()
+//                    .permitAll();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+//                      Criar usuario inicial.
+//        UserDetails user =
+//                User.builder()
+//                        .username("paulo")
+//                        .password(bCryptPasswordEncoder.encode("paulo"))
+//                        .roles("ADM")
+//                        .build();
+
+        auth
+
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(bCryptPasswordEncoder);
+//                .withUser(user);
+
+
+    }
 
 
 }
--- src/main/java/br/com/challange/videos/videos/model/Usuario.java
+++ src/main/java/br/com/challange/videos/videos/model/Usuario.java
@@ -1,41 +1,39 @@
 package br.com.challange.videos.videos.model;
 
-import javax.annotation.processing.Generated;
 import javax.persistence.*;
-//
-//@Entity
-//@Table(name = "users")
+
+@Entity
+@Table(name = "users")
 public class Usuario {
-//    @Id
-//    private String username;
-//    private String password;
-//
-//    public String getUsername() {
-//        return username;
+    @Id
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public Usuario() {
+    }
+
+//    public Usuario(String nome, String email, String senha) {
+//        this.nome = nome;
+//        this.email = email;
+//        this.senha = senha;
 //    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//
-//
-//    public Usuario() {
-//    }
-//
-////    public Usuario(String nome, String email, String senha) {
-////        this.nome = nome;
-////        this.email = email;
-////        this.senha = senha;
-////    }
 
 
 }
